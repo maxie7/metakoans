81569f5a336e85c38a8950ad6aa14d8f8fdd0809
Max Kos <maxie7@protonmail.com>
2016-04-18 12:55:47 +0300

    Pass koan_1 .. koan_5

---
+++ knowledge.rb
@@ -0,0 +1,13 @@
+class Module
+  def attribute(atr, by_default=nil, &blok)
+
+    define_method("#{atr}=") {|x| instance_variable_set "@#{atr}", x }
+
+    define_method(atr) {
+      instance_variable_get "@#{atr}"
+    }
+
+    define_method("#{atr}?") { not send(atr).nil? }
+
+  end
+end
