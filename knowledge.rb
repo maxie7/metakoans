65ff9e9887bc9d0b74901ca60cab655bf43dae6b
Max Kos <maxie7@protonmail.com>
2016-04-18 13:07:11 +0300

    1st try of false assert in koan_6

--- knowledge.rb
+++ knowledge.rb
@@ -1,5 +1,8 @@
 class Module
   def attribute(atr, by_default=nil, &blok)
+    if atr.is_a? (Hash)
+      return atr.each { |atr, by_default| attribute atr, by_default }
+    end
 
     define_method("#{atr}=") {|x| instance_variable_set "@#{atr}", x }
 
