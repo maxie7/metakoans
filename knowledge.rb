187cad10043379892a0ea0384ed63526cde784be
Max Kos <maxie7@protonmail.com>
2016-04-18 13:16:44 +0300

    Wow! Passed...

--- knowledge.rb
+++ knowledge.rb
@@ -6,9 +6,14 @@ class Module
 
     define_method("#{atr}=") {|x| instance_variable_set "@#{atr}", x }
 
-    define_method(atr) { instance_variable_get "@#{atr}" }
+    define_method(atr) {
+      if !instance_variable_defined? "@#{atr}"
+        send "#{atr}=", block_given? ? instance_eval(&blok) : by_default
+      end
+      instance_variable_get "@#{atr}"
+    }
 
-    define_method("#{atr}?") { not send(atr).nil? }
+    define_method("#{atr}?") { !send(atr).nil? }
 
   end
 end
